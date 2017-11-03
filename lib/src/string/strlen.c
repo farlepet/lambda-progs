64f1ab4919d0da1cecbac9147c971b32e6af7b5c
Peter Farley <far.peter1@gmail.com>
2017-11-03 07:32:19 -0500

    Separated lambda-lib, included as a submodule now.

--- lib/src/string/strlen.c
+++
@@ -1,8 +0,0 @@
-#include <unistd.h>
-
-size_t strlen(const char *str)
-{
-	register size_t i = 0;
-	while(str[i]) i++;
-	return i;
-}
--- lib/src/wchar/wcslen.c
+++
@@ -1,8 +0,0 @@
-#include <wchar.h>
-
-size_t  wcslen(const wchar_t *str)
-{
-	register size_t i = 0;
-	while(str[i++]);
-	return i;
-}
